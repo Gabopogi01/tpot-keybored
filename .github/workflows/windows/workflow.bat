ba8a6f1252eb738edb223126a241187de1652231
Gabopogi01 <gabocody@gmail.com>
2026-09-20 18:23:02 +0800

    fix flow

---
+++ .github/workflows/windows/workflow.bat
@@ -0,0 +1,10 @@
+set "THING=flixel haxeui-flixel haxeui-core hxWindowColorMode haxeui-theme-kenney"
+
+echo !!SIGNAL DOWNLOAD LIB
+echo !!THIS IS FOR WORKFLOW ONLY!!
+for %%i in (%THING%) do (
+    haxelib --quiet install %%i
+)
+echo.
+echo done, use %~nx0 --build or --buildtest on ./buildwindow.bat!
+exit /b
